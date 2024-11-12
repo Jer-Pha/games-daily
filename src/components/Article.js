af593f72a1fcd024fda543a2cce2d9151ed00bbf
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-11 20:31:46 -0800

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import React from "react";
 
 export default function Article({
@@ -7,6 +7,7 @@ export default function Article({
   onArticleClick,
   isSelected,
   handleClose,
+  innerRef,
 }) {
   const handleClick = () => {
     if (isSelected) {
@@ -17,15 +18,19 @@ export default function Article({
   };
 
   return (
-    <article className={`${addClasses} cursor-pointer`} onClick={handleClick}>
+    <article
+      className={`${addClasses} w-screen min-w-[100vw] tablet:min-w-[article-card] cursor-pointer pb-1.5 overflow-hidden border-2 border-transparent snap-start snap-always`}
+      onClick={handleClick}
+      ref={innerRef}
+    >
       <img
         src={item.image}
         alt={item.headline}
-        className="w-full aspect-video object-cover max-h-[article-img]"
+        className="w-full aspect-video object-cover tablet:max-h-[article-img] border-b-2 border-transparent"
         loading="lazy"
       />
       <div className="flex flex-col">
-        <h3 className="py-1 px-1.5 text-sm font-semibold line-clamp-2 align-middle">
+        <h3 className="pt-1 px-1.5 text-sm font-semibold line-clamp-2 align-middle">
           {item.headline}
         </h3>
       </div>
