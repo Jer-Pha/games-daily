61b82634e46483a886698cfb669536c0eba47d63
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-11 20:31:31 -0800

    Update Constants.js

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -5,8 +5,12 @@ export const ARROW_OPACITY =
   "text-opacity-45 border-opacity-45 bg-opacity-35 hover:text-opacity-70" +
   " hover:border-opacity-60 hover:bg-opacity-60";
 
-export const SCROLL_ARROWS_TOP =
-  Math.floor((IMAGE_HEIGHT - 32) / 2) + 48 - SCROLL_ARROWS_BORDER;
+export const SCROLL_ARROWS_TOP_DEFAULT = `calc(${Math.floor(
+  ((100 / 16) * 9) / 2
+)}vw - 16px + 48px - ${SCROLL_ARROWS_BORDER}px)`;
+export const SCROLL_ARROWS_TOP_TABLET = `${
+  Math.floor((IMAGE_HEIGHT - 32) / 2) + 48 - SCROLL_ARROWS_BORDER
+}px`;
 /*
 The buttons are 32px tall, defined in the SVG files and the header
 above the image is 48px (may have to worry about responsive sizes later)
