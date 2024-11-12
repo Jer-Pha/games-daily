d213c34fd574f1983bbff81c5b3012a3ee2dd851
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-11 20:31:16 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,20 @@ export default function App() {
     }
   }, []);
 
+  const toggleMode = () => {
+    document.documentElement.classList.toggle("dark");
+  };
+
   return (
     <div className="App">
       <main className="max-w-[1400px] mx-auto bg-[var(--bg-color)]">
         <ArticleHub searchQuery={""} />
+        <button
+          className="fixed top-0 left-0 px-2 py-1 m-4 bg-[var(--bg-color)] rounded-md"
+          onClick={toggleMode}
+        >
+          Toggle
+        </button>
       </main>
     </div>
   );
