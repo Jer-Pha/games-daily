8e28266ece0c2bc71d6a513cd9e188e59b8882aa
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-14 00:38:20 -0800

    Add site icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -7,6 +7,35 @@ import { ReactComponent as CloseIcon } from "../img/svg/close.svg";
 import { ReactComponent as QuestionMarkIcon } from "../img/svg/question-mark.svg";
 import { ReactComponent as ModeToggleIcon } from "../img/svg/mode_toggle.svg";
 
+// Site Logos
+import { ReactComponent as destructoid } from "../img/svg/sites/destructoid.svg";
+import { ReactComponent as dualshockers } from "../img/svg/sites/dualshockers.svg";
+import { ReactComponent as engadget } from "../img/svg/sites/engadget.svg";
+import { ReactComponent as eurogamer } from "../img/svg/sites/eurogamer.svg";
+import { ReactComponent as gamedeveloper } from "../img/svg/sites/gamedeveloper.svg";
+import { ReactComponent as gamerant } from "../img/svg/sites/gamerant.svg";
+import { ReactComponent as gameranx } from "../img/svg/sites/gameranx.svg";
+import { ReactComponent as gamesbeat } from "../img/svg/sites/gamesbeat.svg";
+import { ReactComponent as gamesindustrybiz } from "../img/svg/sites/gamesindustrybiz.svg";
+import { ReactComponent as gamespot } from "../img/svg/sites/gamespot.svg";
+import { ReactComponent as gamesradar } from "../img/svg/sites/gamesradar.svg";
+import { ReactComponent as gamingbolt } from "../img/svg/sites/gamingbolt.svg";
+import { ReactComponent as gematsu } from "../img/svg/sites/gematsu.svg";
+import { ReactComponent as hardcoregamer } from "../img/svg/sites/hardcoregamer.svg";
+import { ReactComponent as ign } from "../img/svg/sites/ign.svg";
+import { ReactComponent as indiegamesplus } from "../img/svg/sites/indiegamesplus.svg";
+import { ReactComponent as kotaku } from "../img/svg/sites/kotaku.svg";
+import { ReactComponent as nintendolife } from "../img/svg/sites/nintendolife.svg";
+import { ReactComponent as pcgamer } from "../img/svg/sites/pcgamer.svg";
+import { ReactComponent as polygon } from "../img/svg/sites/polygon.svg";
+import { ReactComponent as purexbox } from "../img/svg/sites/purexbox.svg";
+import { ReactComponent as pushsquare } from "../img/svg/sites/pushsquare.svg";
+import { ReactComponent as rps } from "../img/svg/sites/rps.svg";
+import { ReactComponent as theescapist } from "../img/svg/sites/theescapist.svg";
+import { ReactComponent as thegamer } from "../img/svg/sites/thegamer.svg";
+import { ReactComponent as vg247 } from "../img/svg/sites/vg247.svg";
+import { ReactComponent as vgc } from "../img/svg/sites/vgc.svg";
+
 export {
   ArrowDropDownIcon,
   ArrowDropUpIcon,
@@ -15,4 +44,31 @@ export {
   CloseIcon,
   ModeToggleIcon,
   QuestionMarkIcon,
+  destructoid,
+  dualshockers,
+  engadget,
+  eurogamer,
+  gamedeveloper,
+  gamerant,
+  gameranx,
+  gamesbeat,
+  gamesindustrybiz,
+  gamespot,
+  gamesradar,
+  gamingbolt,
+  gematsu,
+  hardcoregamer,
+  ign,
+  indiegamesplus,
+  kotaku,
+  nintendolife,
+  pcgamer,
+  polygon,
+  purexbox,
+  pushsquare,
+  rps,
+  theescapist,
+  thegamer,
+  vg247,
+  vgc,
 };
