be5d86d8c35745142062478c2350ddf8bca3dcdb
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-19 21:31:22 -0800

    Remove unused import

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { ArticleContext } from "../context/ArticleContext";
 import Article from "./Article";
 
