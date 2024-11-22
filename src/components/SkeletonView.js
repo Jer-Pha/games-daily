fac5b82b27fab42477fe096f2486ac8ee2280bc0
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-21 19:15:02 -0800

    Fix skeleton view to match major update

--- src/components/SkeletonView.js
+++ src/components/SkeletonView.js
@@ -5,8 +5,8 @@ import { ModeToggleIcon } from "./Icons";
 export default function SkeletonView({ sliceSize }) {
   return (
     <>
-      <div className="tablet:w-[content-t] desktop:w-[content-d] max-w-[content-t] desktop:mx-auto">
-        <div className="flex bg-transparent">
+      <div className="flex flex-col desktop:items-center w-full overflow-y-auto gutter pr-1 tablet:pr-0">
+        <div className="flex bg-transparent tablet:w-[content-t] desktop:w-[content-d] max-w-screen-desktop border-r-[1px] border-transparent tab-corners">
           <button
             className={`px-6 py-3 tablet:py-2 text-center uppercase text-sm bg-[var(--primary-color)] rounded-t-lg font-semibold border-b-0 border-[1px] border-[var(--text-color)]`}
             disabled={true}
@@ -32,28 +32,28 @@ export default function SkeletonView({ sliceSize }) {
             </div>
           </div>
         </div>
-        <div className="flex overflow-y-scroll ax-h-[calc(100vh-58px)] tablet:max-h-[calc(100vh-42px)] border-t-0 border-[1px] border-[var(--text-color)] bg-[var(--primary-color)]">
-          <div className="bg-[var(--primary-color)] overflow-hidden ">
-            {Array.from({ length: sliceSize }).map((_, index) => (
-              <section
-                key={index}
-                className={`tablet:px-4 pb-4 relative ${
-                  index % 2 !== 0 ? "bg-[var(--surface-color)]" : ""
-                }`}
-              >
-                <h2 className="text-xl tablet:text-2xl font-semibold text-left py-2 pl-4 tablet:px-0">
-                  &nbsp;
-                </h2>
-                <div className="overflow-x-auto overflow-y-hidden scrollbar-hide articles-container snap-x snap-mandatory">
-                  <div className="flex gap-0 tablet:gap-1 w-max article-row">
-                    {Array.from({ length: 5 }).map((_, index) => (
-                      <SkeletonArticle key={index} />
-                    ))}
-                  </div>
+      </div>
+      <div className="flex flex-col w-full desktop:items-center overflow-x-hidden overflow-y-auto gutter max-h-[calc(100vh-58px)] tablet:max-h-[calc(100vh-42px)] pr-1 tablet:pr-0">
+        <div className="content-view tablet:w-[content-t] desktop:w-[content-d] max-w-screen-desktop border-content">
+          {Array.from({ length: sliceSize }).map((_, index) => (
+            <section
+              key={index}
+              className={`tablet:px-4 pb-4 relative ${
+                index % 2 !== 0 ? "bg-[var(--surface-color)]" : ""
+              }`}
+            >
+              <h2 className="text-xl tablet:text-2xl font-semibold text-left py-2 pl-4 tablet:px-0">
+                &nbsp;
+              </h2>
+              <div className="overflow-x-auto overflow-y-hidden scrollbar-hide articles-container snap-x snap-mandatory">
+                <div className="flex gap-0 tablet:gap-1 w-max article-row">
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <SkeletonArticle key={index} />
+                  ))}
                 </div>
-              </section>
-            ))}
-          </div>
+              </div>
+            </section>
+          ))}
         </div>
       </div>
     </>
