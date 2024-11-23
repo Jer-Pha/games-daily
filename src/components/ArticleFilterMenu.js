79f457bc05bac49616a5b8129681e7331d964eb7
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-11-23 14:59:42 -0800

    Add responsive features to filter menu

--- src/components/ArticleFilterMenu.js
+++ src/components/ArticleFilterMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { sanitizeId } from "../utils/utils";
 import Fuse from "fuse.js";
-import { CloseIcon } from "./Icons";
+import { CloseIcon, FilterOffIcon, FilterOnIcon } from "../utils/Icons";
 
 export default function ArticleFilterMenu({
   outletNames,
@@ -13,6 +13,7 @@ export default function ArticleFilterMenu({
 }) {
   const [activeTab, setActiveTab] = useState("topic");
   const [searchQuery, setSearchQuery] = useState("");
+  const [filterMenuIsOpen, setFilterMenuIsOpen] = useState(false);
 
   // Add initials of topics to search parameters
   const topicDataWithInitials = topicData.map((originalTopic) => {
@@ -90,125 +91,159 @@ export default function ArticleFilterMenu({
     }
   };
 
+  // Toggle filter menu visibility
+  const handleFilterMenuToggle = (status) => {
+    setFilterMenuIsOpen(status);
+  };
+
   return (
-    <div className="w-60 desktop:w-80 p-4">
-      <div id="filter-tabs" className="flex">
-        <button
-          type="button"
-          className={`w-1/2 py-2 text-sm bg-[var(--surface-color)] border-[var(--text-color)] rounded-t-md ${
-            activeTab === "topic"
-              ? "border-b-0 border-[1px]"
-              : "border-b-[1px] inner-top-left"
-          }`}
-          onClick={() => handleTabClick("topic")}
-        >
-          Topics
-        </button>
-        <button
-          type="button"
-          className={`w-1/2 py-2 text-sm bg-[var(--primary-color)] border-[var(--text-color)] rounded-t-md ${
-            activeTab === "outlet"
-              ? "border-b-0 border-[1px]"
-              : "border-b-[1px] inner-top-right"
-          }`}
-          onClick={() => handleTabClick("outlet")}
-        >
-          Outlets
-        </button>
-      </div>
+    <div
+      className={`w-full desktop:w-auto h-full fixed desktop:static left-0 top-0 z-40 ${
+        filterMenuIsOpen ? "pointer-events-auto" : "pointer-events-none"
+      }`}
+    >
       <div
-        id="filter-options"
-        className={`px-4 py-2 border-t-0 border-[1px] border-[var(--text-color)] max-h-[calc(100vh-182px)] tablet:max-h-[calc(100vh-166px)] overflow-y-auto overflow-x-hidden text-left ${
-          activeTab === "outlet"
-            ? "bg-[var(--primary-color)]"
-            : "bg-[var(--surface-color)]"
+        id="filter-menu"
+        className={`w-80 h-full desktop:h-auto absolute desktop:static top-0 left-0 z-50 bg-[var(--accent-color)] desktop:bg-[var(--primary-color)] border-r-[var(--text-color)] border-transparent border-[1px] border-b-[1px] p-4 pointer-events-auto transform transition-transform desktop:transform-none ${
+          filterMenuIsOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        {activeTab === "outlet" && (
-          <div className="flex flex-col">
-            {outletNames.map((outlet) => (
-              <label
-                htmlFor={`checkbox-${outlet.slug}`}
-                key={outlet.slug}
-                className="py-0.5 cursor-pointer relative"
-              >
-                <input
-                  id={`checkbox-${outlet.slug}`}
-                  type="checkbox"
-                  value={outlet.slug}
-                  name="filter-by-outlet"
-                  className="absolute top-1/2 transform -translate-y-1/2"
-                  checked={selectedOutlets.includes(outlet.slug)}
-                  onChange={() => handleOutletChange(outlet.slug)}
-                />
-                <span className="ml-6 inline-block">{outlet.name}</span>
-              </label>
-            ))}
+        <button
+          type="button"
+          className="absolute desktop:static desktop:hidden top-1/2 -right-0 transform translate-x-full -translate-y-1/2 p-1 border-[var(--text-color)] border-[1px] border-l-[var(--accent-color)] border-l-2 rounded-r-lg bg-[var(--accent-color)] "
+          onClick={() => handleFilterMenuToggle(!filterMenuIsOpen)}
+        >
+          <div className="h-8 w-8">
+            {filterMenuIsOpen ? <FilterOffIcon /> : <FilterOnIcon />}
           </div>
-        )}
-        {activeTab === "topic" && (
-          <div className="flex flex-col">
-            <div className="relative my-2">
-              <input
-                type="text"
-                placeholder="Search topics..."
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                className="bg-[var(--primary-color)] rounded-lg px-4 py-1 w-full"
-              />
-              {searchQuery && (
-                <button
-                  className="absolute right-2 top-1/2 transform -translate-y-1/2"
-                  onClick={() => setSearchQuery("")}
+        </button>
+        <div id="filter-tabs" className="flex">
+          <button
+            type="button"
+            className={`w-1/2 py-2 text-sm bg-[var(--bg-color)] border-[var(--text-color)] rounded-t-md ${
+              activeTab === "topic"
+                ? "border-b-0 border-[1px]"
+                : "border-b-[1px] inner-top-left"
+            }`}
+            onClick={() => handleTabClick("topic")}
+          >
+            Topics
+          </button>
+          <button
+            type="button"
+            className={`w-1/2 py-2 text-sm bg-[var(--surface-color)] border-[var(--text-color)] rounded-t-md ${
+              activeTab === "outlet"
+                ? "border-b-0 border-[1px]"
+                : "border-b-[1px] inner-top-right"
+            }`}
+            onClick={() => handleTabClick("outlet")}
+          >
+            Outlets
+          </button>
+        </div>
+        <div
+          id="filter-options"
+          className={`px-4 py-2 border-t-0 border-[1px] border-[var(--text-color)] max-h-[calc(100vh-185px)] tablet:max-h-[calc(100vh-170px)] overflow-y-auto overflow-x-hidden text-left ${
+            activeTab === "outlet"
+              ? "bg-[var(--surface-color)]"
+              : "bg-[var(--bg-color)]"
+          }`}
+        >
+          {activeTab === "outlet" && (
+            <div className="flex flex-col">
+              {outletNames.map((outlet) => (
+                <label
+                  htmlFor={`checkbox-${outlet.slug}`}
+                  key={outlet.slug}
+                  className="py-0.5 cursor-pointer relative"
                 >
-                  <CloseIcon className="h-5 w-5 text-gray-500" />
-                </button>
+                  <input
+                    id={`checkbox-${outlet.slug}`}
+                    type="checkbox"
+                    value={outlet.slug}
+                    name="filter-by-outlet"
+                    className="absolute desktop:static top-1/2 transform -translate-y-1/2"
+                    checked={selectedOutlets.includes(outlet.slug)}
+                    onChange={() => handleOutletChange(outlet.slug)}
+                  />
+                  <span className="ml-6 inline-block">{outlet.name}</span>
+                </label>
+              ))}
+            </div>
+          )}
+          {activeTab === "topic" && (
+            <div className="flex flex-col">
+              <div className="relative my-2">
+                <input
+                  type="text"
+                  placeholder="Search topics..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  className="bg-[var(--surface-color)] rounded-lg px-4 py-1 w-full"
+                />
+                {searchQuery && (
+                  <button
+                    className="absolute desktop:static right-2 top-1/2 transform -translate-y-1/2"
+                    onClick={() => setSearchQuery("")}
+                  >
+                    <CloseIcon className="h-5 w-5 text-gray-500" />
+                  </button>
+                )}
+              </div>
+              {filterTopics(topicDataWithInitials, searchQuery).map(
+                (topicObject) => {
+                  const topic = topicObject.topic;
+                  return (
+                    <label
+                      htmlFor={`checkbox-${sanitizeId(topic)}`}
+                      key={topic}
+                      className="py-0.5 cursor-pointer relative"
+                    >
+                      <input
+                        id={`checkbox-${sanitizeId(topic)}`}
+                        type="checkbox"
+                        value={topic}
+                        name="filter-by-topic"
+                        className="absolute desktop:static top-1/2 transform -translate-y-1/2"
+                        checked={selectedTopics.includes(topic)}
+                        onChange={() => handleTopicChange(topic)}
+                      />
+                      <span className="ml-6 inline-block">{topic}</span>
+                    </label>
+                  );
+                }
               )}
             </div>
-            {filterTopics(topicDataWithInitials, searchQuery).map(
-              (topicObject) => {
-                const topic = topicObject.topic;
-                return (
-                  <label
-                    htmlFor={`checkbox-${sanitizeId(topic)}`}
-                    key={topic}
-                    className="py-0.5 cursor-pointer relative"
-                  >
-                    <input
-                      id={`checkbox-${sanitizeId(topic)}`}
-                      type="checkbox"
-                      value={topic}
-                      name="filter-by-topic"
-                      className="absolute top-1/2 transform -translate-y-1/2"
-                      checked={selectedTopics.includes(topic)}
-                      onChange={() => handleTopicChange(topic)}
-                    />
-                    <span className="ml-6 inline-block">{topic}</span>
-                  </label>
-                );
-              }
-            )}
-          </div>
-        )}
+          )}
+        </div>
+        <div className="flex justify-evenly mt-4">
+          <button
+            id="filter-clear"
+            type="reset"
+            className="px-4 py-2 bg-[var(--text-color)] text-[var(--bg-color)] rounded-lg"
+            onClick={handleClearFilter}
+          >
+            Clear all
+          </button>
+          <button
+            id="filter-all"
+            type="button"
+            className="px-4 py-2 bg-[var(--primary-color)] desktop:bg-[var(--accent-color)] rounded-lg"
+            onClick={handleSelectAllFilter}
+          >
+            Select all
+          </button>
+        </div>
       </div>
-      <div className="flex justify-evenly mt-4">
-        <button
-          id="filter-clear"
-          type="reset"
-          className="px-4 py-2 bg-[var(--text-color)] text-[var(--primary-color)] rounded-lg"
-          onClick={handleClearFilter}
-        >
-          Clear all
-        </button>
-        <button
-          id="filter-all"
-          type="button"
-          className="px-4 py-2 bg-[var(--accent-color)] rounded-lg"
-          onClick={handleSelectAllFilter}
-        >
-          Select all
-        </button>
-      </div>
+      <div
+        id="filter-overlay"
+        className={`w-full h-full bg-black bg-opacity-50 absolute desktop:static top-0 left-0 z-40 ${
+          filterMenuIsOpen
+            ? "static desktop:hidden"
+            : "hidden pointer-events-none"
+        }`}
+        onClick={() => handleFilterMenuToggle(false)}
+      ></div>
     </div>
   );
 }
--- src/components/ArticlesByFilterView.js
+++ src/components/ArticlesByFilterView.js
@@ -3,7 +3,7 @@ import { ArticleContext } from "../context/ArticleContext";
 import Article from "./Article";
 import ArticleFilterMenu from "./ArticleFilterMenu";
 
-import { ArrowLeftWideIcon } from "./Icons";
+import { ArrowLeftWideIcon } from "../utils/Icons";
 
 export default function ArticlesByFilterView({
   allArticles,
@@ -45,58 +45,48 @@ export default function ArticlesByFilterView({
   }, [viewRef, previousView]);
 
   return (
-    <>
-      <div className="block tablet:hidden p-4">
-        <p>
-          Filter view is not available on mobile yet. Please use a larger
-          device.
-        </p>
-      </div>
+    <div
+      ref={viewRef}
+      className="flex min-h-[calc(100vh-58px)] tablet:min-h-[calc(100vh-42px)] content-view tablet:w-[content-t] desktop:w-[content-d] max-w-screen-desktop border-content right bg-[var(--primary-color)]"
+    >
+      <ArticleFilterMenu
+        topicData={topicData}
+        outletNames={outletNames}
+        selectedOutlets={selectedOutlets}
+        setSelectedOutlets={setSelectedOutlets}
+        setSelectedTopics={setSelectedTopics}
+        selectedTopics={selectedTopics}
+      />
       <div
-        ref={viewRef}
-        className="hidden tablet:flex min-h-[calc(100vh-58px)] tablet:min-h-[calc(100vh-42px)] content-view tablet:w-[content-t] desktop:w-[content-d] max-w-screen-desktop border-content right"
+        ref={containerRef}
+        className={`flex flex-wrap justify-around flex-1 overflow-y-auto p-4 ${
+          filteredSections.length === 0
+            ? "min-h-[calc(100vh-90px)] tablet:min-h-[calc(100vh-74px)] content-center"
+            : "content-start"
+        }`}
       >
-        <ArticleFilterMenu
-          topicData={topicData}
-          outletNames={outletNames}
-          selectedOutlets={selectedOutlets}
-          setSelectedOutlets={setSelectedOutlets}
-          setSelectedTopics={setSelectedTopics}
-          selectedTopics={selectedTopics}
-        />
-        <div
-          ref={containerRef}
-          className={`flex flex-wrap justify-around flex-1 overflow-y-auto ${
-            filteredSections.length === 0
-              ? "min-h-[calc(100vh-90px)] tablet:min-h-[calc(100vh-74px)] content-center"
-              : "content-start"
-          }`}
-        >
-          {filteredSections.length > 0 ? (
-            filteredSections.map((article) => (
-              <Article
-                article={article}
-                key={article.id}
-                scrollContainerRef={scrollContainerRef}
-                addClasses={`tablet:max-h-[article-img] tablet:mb-4 w-screen tablet:w-[article-card] ${
-                  selectedArticle && selectedArticle.id === article.id
-                    ? "selected"
-                    : ""
-                }`}
-              />
-            ))
-          ) : (
-            <div className="flex flex-col items-center justify-center w-full">
-              <p className="text-lg p-2">
-                Select a filter option to get started
-              </p>
-              <div className="animate-bounce h-10 w-10">
-                <ArrowLeftWideIcon />
-              </div>
+        {filteredSections.length > 0 ? (
+          filteredSections.map((article) => (
+            <Article
+              article={article}
+              key={article.id}
+              scrollContainerRef={scrollContainerRef}
+              addClasses={`tablet:max-h-[article-img] tablet:mb-4 w-screen tablet:w-[article-card] ${
+                selectedArticle && selectedArticle.id === article.id
+                  ? "selected"
+                  : ""
+              }`}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col items-center justify-center w-full">
+            <p className="text-lg p-2">Select a filter option to get started</p>
+            <div className="animate-bounce h-10 w-10">
+              <ArrowLeftWideIcon />
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
