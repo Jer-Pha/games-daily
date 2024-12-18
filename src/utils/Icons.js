540db39d04d413854a33e8a75822546236ac4d41
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-12-17 17:05:42 -0800

    Clean up import/export statements

--- src/utils/Icons.js
+++ src/utils/Icons.js
@@ -1,84 +1,43 @@
 // UI
-import { ReactComponent as ArrowDropDownIcon } from "../img/svg/arrow-drop-down.svg";
-import { ReactComponent as ArrowDropUpIcon } from "../img/svg/arrow-drop-up.svg";
-import { ReactComponent as ArrowLeftIcon } from "../img/svg/arrow-left.svg";
-import { ReactComponent as ArrowLeftWideIcon } from "../img/svg/arrow-left-wide.svg";
-import { ReactComponent as ArrowRightIcon } from "../img/svg/arrow-right.svg";
-import { ReactComponent as CloseIcon } from "../img/svg/close.svg";
-import { ReactComponent as FilterOffIcon } from "../img/svg/filter-off.svg";
-import { ReactComponent as FilterOnIcon } from "../img/svg/filter-on.svg";
-import { ReactComponent as InfoIcon } from "../img/svg/info.svg";
-import { ReactComponent as LogoIcon } from "../img/svg/logo.svg";
-import { ReactComponent as ModeToggleIcon } from "../img/svg/mode_toggle.svg";
-import { ReactComponent as QuestionMarkIcon } from "../img/svg/question-mark.svg";
+export { ReactComponent as ArrowDropDownIcon } from "../img/svg/arrow-drop-down.svg";
+export { ReactComponent as ArrowDropUpIcon } from "../img/svg/arrow-drop-up.svg";
+export { ReactComponent as ArrowLeftIcon } from "../img/svg/arrow-left.svg";
+export { ReactComponent as ArrowLeftWideIcon } from "../img/svg/arrow-left-wide.svg";
+export { ReactComponent as ArrowRightIcon } from "../img/svg/arrow-right.svg";
+export { ReactComponent as CloseIcon } from "../img/svg/close.svg";
+export { ReactComponent as ExternalLinkIcon } from "../img/svg/external-link.svg";
+export { ReactComponent as FilterOffIcon } from "../img/svg/filter-off.svg";
+export { ReactComponent as FilterOnIcon } from "../img/svg/filter-on.svg";
+export { ReactComponent as InfoIcon } from "../img/svg/info.svg";
+export { ReactComponent as LogoIcon } from "../img/svg/logo.svg";
+export { ReactComponent as ModeToggleIcon } from "../img/svg/mode_toggle.svg";
+export { ReactComponent as QuestionMarkIcon } from "../img/svg/question-mark.svg";
 
 // Site Logos
-import { ReactComponent as destructoid } from "../img/svg/sites/destructoid.svg";
-import { ReactComponent as dualshockers } from "../img/svg/sites/dualshockers.svg";
-import { ReactComponent as engadget } from "../img/svg/sites/engadget.svg";
-import { ReactComponent as eurogamer } from "../img/svg/sites/eurogamer.svg";
-import { ReactComponent as gamedeveloper } from "../img/svg/sites/gamedeveloper.svg";
-import { ReactComponent as gamerant } from "../img/svg/sites/gamerant.svg";
-import { ReactComponent as gameranx } from "../img/svg/sites/gameranx.svg";
-import { ReactComponent as gamesbeat } from "../img/svg/sites/gamesbeat.svg";
-import { ReactComponent as gamesindustrybiz } from "../img/svg/sites/gamesindustrybiz.svg";
-import { ReactComponent as gamespot } from "../img/svg/sites/gamespot.svg";
-import { ReactComponent as gamesradar } from "../img/svg/sites/gamesradar.svg";
-import { ReactComponent as gamingbolt } from "../img/svg/sites/gamingbolt.svg";
-import { ReactComponent as gematsu } from "../img/svg/sites/gematsu.svg";
-import { ReactComponent as hardcoregamer } from "../img/svg/sites/hardcoregamer.svg";
-import { ReactComponent as ign } from "../img/svg/sites/ign.svg";
-import { ReactComponent as indiegamesplus } from "../img/svg/sites/indiegamesplus.svg";
-import { ReactComponent as kotaku } from "../img/svg/sites/kotaku.svg";
-import { ReactComponent as nintendolife } from "../img/svg/sites/nintendolife.svg";
-import { ReactComponent as pcgamer } from "../img/svg/sites/pcgamer.svg";
-import { ReactComponent as polygon } from "../img/svg/sites/polygon.svg";
-import { ReactComponent as purexbox } from "../img/svg/sites/purexbox.svg";
-import { ReactComponent as pushsquare } from "../img/svg/sites/pushsquare.svg";
-import { ReactComponent as rps } from "../img/svg/sites/rps.svg";
-import { ReactComponent as theescapist } from "../img/svg/sites/theescapist.svg";
-import { ReactComponent as thegamer } from "../img/svg/sites/thegamer.svg";
-import { ReactComponent as vg247 } from "../img/svg/sites/vg247.svg";
-import { ReactComponent as vgc } from "../img/svg/sites/vgc.svg";
-
-export {
-  ArrowDropDownIcon,
-  ArrowDropUpIcon,
-  ArrowLeftIcon,
-  ArrowLeftWideIcon,
-  ArrowRightIcon,
-  CloseIcon,
-  FilterOffIcon,
-  FilterOnIcon,
-  InfoIcon,
-  LogoIcon,
-  ModeToggleIcon,
-  QuestionMarkIcon,
-  destructoid,
-  dualshockers,
-  engadget,
-  eurogamer,
-  gamedeveloper,
-  gamerant,
-  gameranx,
-  gamesbeat,
-  gamesindustrybiz,
-  gamespot,
-  gamesradar,
-  gamingbolt,
-  gematsu,
-  hardcoregamer,
-  ign,
-  indiegamesplus,
-  kotaku,
-  nintendolife,
-  pcgamer,
-  polygon,
-  purexbox,
-  pushsquare,
-  rps,
-  theescapist,
-  thegamer,
-  vg247,
-  vgc,
-};
+export { ReactComponent as destructoid } from "../img/svg/sites/destructoid.svg";
+export { ReactComponent as dualshockers } from "../img/svg/sites/dualshockers.svg";
+export { ReactComponent as engadget } from "../img/svg/sites/engadget.svg";
+export { ReactComponent as eurogamer } from "../img/svg/sites/eurogamer.svg";
+export { ReactComponent as gamedeveloper } from "../img/svg/sites/gamedeveloper.svg";
+export { ReactComponent as gamerant } from "../img/svg/sites/gamerant.svg";
+export { ReactComponent as gameranx } from "../img/svg/sites/gameranx.svg";
+export { ReactComponent as gamesbeat } from "../img/svg/sites/gamesbeat.svg";
+export { ReactComponent as gamesindustrybiz } from "../img/svg/sites/gamesindustrybiz.svg";
+export { ReactComponent as gamespot } from "../img/svg/sites/gamespot.svg";
+export { ReactComponent as gamesradar } from "../img/svg/sites/gamesradar.svg";
+export { ReactComponent as gamingbolt } from "../img/svg/sites/gamingbolt.svg";
+export { ReactComponent as gematsu } from "../img/svg/sites/gematsu.svg";
+export { ReactComponent as hardcoregamer } from "../img/svg/sites/hardcoregamer.svg";
+export { ReactComponent as ign } from "../img/svg/sites/ign.svg";
+export { ReactComponent as indiegamesplus } from "../img/svg/sites/indiegamesplus.svg";
+export { ReactComponent as kotaku } from "../img/svg/sites/kotaku.svg";
+export { ReactComponent as nintendolife } from "../img/svg/sites/nintendolife.svg";
+export { ReactComponent as pcgamer } from "../img/svg/sites/pcgamer.svg";
+export { ReactComponent as polygon } from "../img/svg/sites/polygon.svg";
+export { ReactComponent as purexbox } from "../img/svg/sites/purexbox.svg";
+export { ReactComponent as pushsquare } from "../img/svg/sites/pushsquare.svg";
+export { ReactComponent as rps } from "../img/svg/sites/rps.svg";
+export { ReactComponent as theescapist } from "../img/svg/sites/theescapist.svg";
+export { ReactComponent as thegamer } from "../img/svg/sites/thegamer.svg";
+export { ReactComponent as vg247 } from "../img/svg/sites/vg247.svg";
+export { ReactComponent as vgc } from "../img/svg/sites/vgc.svg";
