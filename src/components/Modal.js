1be6c94bc05a152a3054e518f33ee393a7327acd
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-12-17 17:04:58 -0800

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,7 +12,7 @@ export default function Modal() {
   return (
     <div className="fixed inset-0 z-[999] flex items-center justify-center">
       <div className="fixed inset-0 bg-black opacity-60" onClick={closeModal} />
-      <div className="bg-[var(--surface-color)] rounded-lg p-4 z-50 max-w-80 tablet:max-w-96 relative border-[var(--text-color)] border-2">
+      <div className="bg-[var(--surface-color)] rounded-lg p-4 z-50 max-w-80 tablet:max-w-96 max-h-screen relative border-[var(--text-color)] overflow-y-auto border-2">
         <button
           onClick={closeModal}
           className="absolute right-2 top-2 h-6 w-6"
